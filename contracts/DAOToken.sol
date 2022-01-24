92b092f033703b8b419d130f982444350bf1eed3
DavidYu1010 <provic0099@gmail.com>
2022-01-24 23:51:02 +0800

    initialize DAO Token contract

---
+++ contracts/DAOToken.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract DAOToken is ERC20{
+    constructor() ERC20("VenymDAO", "VNM"){
+        _mint(msg.sender, 10000000000000);
+    }
+}
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const DAOToken = artifacts.require("DAOToken");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(DAOToken);
 };
