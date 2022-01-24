f5942b6f300d12ca7950f6c1369202249e393e29
DavidYu1010 <provic0099@gmail.com>
2022-01-25 02:22:33 +0800

    add unit testing

---
+++ test/Test.js
@@ -0,0 +1,18 @@
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
+const {assert} = require('chai')
+
+const DAOToken = artifacts.require('./DAOToken.sol')
+
+contract('Burn Token Contract', (accounts) => {
+    let daoToken, res
+    before(async() => {
+        daoToken = await DAOToken.deployed()
+    })
+    it('Create Token Proposal', async() => {
+        res = await daoToken.totalSupply()
+        console.log(res.toString())
+    })
+})
